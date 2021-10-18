99fac2cabecc75101a5fbd4c469351ce11deae31
MitschlLUL <michel.hoffmeister@web.de>
2021-10-18 22:43:30 +0200

    [TASK] added the add device popup to frontend

--- WebServerTest/data/index.js
+++ WebServerTest/data/index.js
@@ -1,3 +1,6 @@
+//id for devices
+let deviceID = 0;
+
 //video constraints
 let constraints = { video: { width: window.outerWidth / 2 }};
 let video = document.querySelector('video');
@@ -6,6 +9,39 @@ let context = canvas.getContext('2d');
 let w, h, ratio;
 let position = 0;
 let focus = 0;
+
+//popup
+const openPopupButtons = document.querySelectorAll("[data-popup-target]");
+const closePopupButtons = document.querySelectorAll("[data-close-button]");
+const overlay= document.getElementById('popupOverlay');
+
+//popup event listeners
+openPopupButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		const popup = document.querySelector(button.dataset.popupTarget);
+		openPopup(popup);
+	})
+})
+
+closePopupButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		const popup = button.closest(".addPopup");
+		closePopup(popup);
+	})
+})
+
+function openPopup(popup){
+	if(popup == null) return;
+	popup.classList.add("active");
+	overlay.classList.add("active");
+}
+
+function closePopup(popup){
+	if(popup == null) return;
+	popup.classList.remove("active");
+	overlay.classList.remove("active");
+}
+
 
 //push the camera input into the liveVideo
 navigator.mediaDevices.getUserMedia(constraints)
@@ -59,4 +95,57 @@ function turnLEDOn() {
 	}).then(res => {
 		console.log("Post request complete! response:", res)
 	});
+}
+
+//changes the displayed number of Pins based on the selected device
+function displayPinInputs(){
+	let pinInputs = document.getElementById("devicePins");
+	if(document.getElementById("selectDevice").value == "LED"){
+		//remove all pin inputs then add one new one for the LED
+		var child = pinInputs.lastElementChild;
+		while(child){
+			pinInputs.removeChild(child);
+			child = pinInputs.lastElementChild;
+		}
+		let device = document.createElement("div");
+		device.className = "centered";
+		let input = document.createElement("input");
+		input.id = "ledPinInput1";
+		pinInputs.appendChild(device);
+		device.appendChild(input);
+	} else if(document.getElementById("selectDevice").value == "Motor"){
+		//remove all pin inputs then add 4 new one for a motor
+		var child = pinInputs.lastElementChild;
+		while(child){
+			pinInputs.removeChild(child);
+			child = pinInputs.lastElementChild;
+		}
+		let device = document.createElement("div");
+		device.className = "block";
+		let div1 = document.createElement("div");
+		div1.className = "centered";
+		let input1 = document.createElement("input");
+		input1.id = "motorPinInput1";	
+		let div2 = document.createElement("div");
+		div2.className = "centered";
+		let input2 = document.createElement("input");
+		input2.id = "motorPinInput2";
+		let div3 = document.createElement("div");
+		div3.className = "centered";
+		let input3 = document.createElement("input");
+		input3.id = "motorPinInput3";
+		let div4 = document.createElement("div");
+		div4.className = "centered";
+		let input4 = document.createElement("input");
+		input4.id = "motorPinInput4";
+		pinInputs.appendChild(device);
+		div1.appendChild(input1);
+		div2.appendChild(input2);
+		div3.appendChild(input3);
+		div4.appendChild(input4);
+		device.appendChild(div1);
+		device.appendChild(div2);
+		device.appendChild(div3);
+		device.appendChild(div4);
+	}
 }
